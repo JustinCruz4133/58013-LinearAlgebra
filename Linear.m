5525e8370573281f90b3bf9d2d8651009bbb0c67
JustinCruz4133 <123426818+JustinCruz4133@users.noreply.github.com>
2023-10-02 08:36:57 +0800

    Add files via upload

---
+++ Linear.m
@@ -0,0 +1,3 @@
+A = [1,1,1;1,2,2;1,2,3], A1 = [5,1,1;6,2,2;8,2,3], A2 = [1,5,1;1,6,2;1,8,3], A3 = [1,1,5;1,2,6;1,2,8]
+Adet = det(A), A1det = det(A1), A2det = det(A2), A3det = det(A3)
+x1 = A1det / Adet, x2 = A2det / Adet, x3 = A3det / Adet
