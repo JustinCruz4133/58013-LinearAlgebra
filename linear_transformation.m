4986d68110495c7743c935ce48d5e3e9818654cc
JustinCruz4133 <123426818+JustinCruz4133@users.noreply.github.com>
2023-11-20 09:16:36 +0800

    Create linear_transformation.m

---
+++ linear_transformation.m
@@ -0,0 +1,10 @@
+syms x y z;
+eq1 = 4*x + 3*y + 2*z == 25;
+eq2 = -2*x + 2*y + 3*z == -10;
+eq3 = 3*x - 5*y + 2*z == -4;
+[A, B] = equationsToMatrix([eq1, eq2, eq3], [x, y, z]);
+D = linsolve(A, B);
+A; B; D;
+
+plot(D);
+plot(A, B);
